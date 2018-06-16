2d1f73f6de849bc552d59878cc5e30d9ef00c315
Gal Huta <galhuta@gmail.com>
2018-06-16 18:10:38 +0300

    notification works!

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { navItems } from './../../_nav';
+import {Router} from '@angular/router';
+import {UserService} from '../../shared/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,8 +13,9 @@ export class DefaultLayoutComponent implements OnInit{
   private changes: MutationObserver;
   public element: HTMLElement = document.body;
   public userPicture = "http://grigale.grigale.com/fitness360user_app/user_images/";
+  numberOfMealsToUpdate = '';
 
-  constructor() {
+  constructor(private router: Router,private userService: UserService) {
 
     this.changes = new MutationObserver((mutations) => {
       this.sidebarMinimized = document.body.classList.contains('sidebar-minimized')
@@ -25,5 +28,18 @@ export class DefaultLayoutComponent implements OnInit{
 
   ngOnInit() {
     this.userPicture = localStorage.getItem('picture');
+    this.userService.get_pending_meals_images(localStorage.getItem('email'))
+      .subscribe((data: any) => {
+          if (data.error == true) {
+            alert('Error!');
+          } else {
+            if (data.length > 0) {
+              this.numberOfMealsToUpdate = data.length;
+            }
+          }
+        },
+        err => {
+          console.log('Error: ' + err.error);
+        });
   }
 }
